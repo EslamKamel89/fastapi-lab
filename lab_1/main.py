abdf15b685c68b600a30ad454c977f1cdc0380b8
EslamKamel89 <elsamkamelforex@gmail.com>
2026-08-22 23:31:16 +0300

    feat(lab-1): explore Pydantic validation and item models

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,69 +1,47 @@
+import json
 from typing import Annotated
 
 import uvicorn
-from fastapi import FastAPI, HTTPException, Path, Query
-from pydantic import BaseModel
+from fastapi import FastAPI
+from pydantic import BaseModel, Field, StringConstraints, validate_call
+from pydantic_core._pydantic_core import ValidationError
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/health")
-async def health():
-    return {"status": "ok"}
-
-
 class Item(BaseModel):
-    id: int
-    name: str
+    name: str = Field(..., min_length=3)
+    description: str | None = None
     price: float
-    tax: float | None = None
 
 
-items = [
-    Item(
-        id=i,
-        name=f"Product-{i}",
-        price=i * 100,
-        tax=i / 10,
-    )
-    for i in range(1, 20)
-]
+@validate_call
+def validate_name(name: Annotated[str, StringConstraints(min_length=4)]):
+    return name
 
 
-@app.get("/items/get/{item_id}", response_model=Item)
-async def get_item_by_id(item_id: Annotated[int, Path(ge=1)]):
-    for item in items:
-        if item.id == item_id:
-            return item
-    raise HTTPException(status_code=404, detail="Product not found")
+@app.get("/test")
+async def test():
+    try:
+        name = validate_name("Ali")
+        print("name is valid")
+        return name
+    except ValidationError as e:
+        print("Name not valid")
+        print(e)
+        return json.loads(e.json())
 
 
-@app.get("/items/all", response_model=list[Item])
-async def get_items(
-    skip: Annotated[int, Query(ge=0)], limit: Annotated[int, Query(ge=1)]
-):
-    return items[skip : skip + limit]
-
-
-@app.get("/items/{item_name}")
-async def get_item(
-    item_name: Annotated[str, Path(min_length=3, max_length=100)],
-    company: Annotated[str, Query()],
-    count: Annotated[int, Query(ge=1)] = 1,
-    rating: Annotated[int | None, Query(ge=1, le=5)] = None,
-):
-    return {
-        "item": item_name,
-        "company": company,
-        "count": count,
-        "rating": rating if rating else "You didn't leave a rating",
-    }
+@app.post("/item", response_model=Item)
+async def create_item(item: Item):
+    print(item.model_dump(exclude={"price"}))
+    return item
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+    )
